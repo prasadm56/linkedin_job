525356fc17f231fe282615995f0acbede9b7f136
Prasad Mukkawar <47525814+prasadm56@users.noreply.github.com>
2024-05-24 23:33:51 +0530

    Update anvil.py

--- anvil.py
+++ anvil.py
@@ -71,7 +71,7 @@ def main(url1, url2):
             job_alerts.append(alert)
 
     job_alert_text ="Day X of Job Postings: Helping Job Seekers Find Opportunities!"
-    job_alert_text += "\n 📢 Job Openings Alert! 📢
+    job_alert_text += "\n 📢 Job Openings Alert! 📢"
     job_alert_text += "\n Exciting opportunities for Data Scientists, Business Analysts, and Data Analysts with 0-2 years of experience!\n \n "
     job_alert_text += "\n".join(job_alerts)
     job_alert_text += "\n Follow @Prasad Mukkawar For More Daily Job Updates 😊"
