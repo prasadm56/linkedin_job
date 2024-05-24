132f34998458cf6c775088b286b2fa65406ddbe6
Prasad Mukkawar <47525814+prasadm56@users.noreply.github.com>
2024-05-24 23:56:22 +0530

    Update anvil.py

--- anvil.py
+++ anvil.py
@@ -78,7 +78,7 @@ def main(url1, url2):
     job_alert_text += "\n Exciting opportunities for Data Scientists, Business Analysts, and Data Analysts with 0-2 years of experience!"
     job_alert_text += "\n"
     job_alert_text += "\n"
-    job_alerts_text += "\n".join(job_alerts)
+    job_alert_text += "\n".join(job_alerts)
     job_alert_text += "\n"
     job_alert_text += "\n Follow @Prasad Mukkawar For More Daily Job Updates 😊"
     job_alert_text += "\n"
